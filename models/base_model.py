7f3d0c9c8af3a1c482f72c75a66db05da23b20e9
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-17 08:47:41 -0400

    Add documentation to all methods

--- models/base_model.py
+++ models/base_model.py
@@ -13,17 +13,34 @@ class BaseModel:
     """
 
     def __init__(self):
+        """
+        Initialize instance attributes
+            Attrs:
+                id (str): instance identity
+                created_at: date of instance creation
+                updated_at: date of instance attrs change
+        """
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
+        """
+        Returns string representation of instance
+        """
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
+        """
+        Changes the time of instance attrs change
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values
+        of __dict__ of the instance including new attrs
+        """
         obj_dict = {}
         obj_dict['__class__'] = self.__class__.__name__
         self.__dict__['created_at'] = (datetime.isoformat(
