ba4aba6b4d6563270ddb4170d1a60902ef91b1f5
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-20 09:57:47 -0400

    Add do show, do all and do destroy to console

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 Entry point of the command interpreter
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -10,20 +12,82 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         'Quit command to exit the program\n'
-        self.close()
-        quit()
-    
+        return True
+
     def close(self):
         return True
-    
+
     def do_EOF(self, arg):
         'EOF command to exit the program'
         self.close()
         print()
         quit()
-    
+
     def emptyline(self):
         pass
+
+    def do_create(self, arg):
+        if not arg:
+            print('** class name missing **')
+            return
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except Exception:
+            print('** class doesn\'t exist **')
+
+    def do_show(self, arg):
+        if not arg:
+            print('** class name missing **')
+            return
+        instance = arg.split()
+        if len(instance) == 1:
+            print('** instance id missing **')
+            return
+        try:
+            Base_id = f'{instance[0]}.{instance[1]}'
+            print(storage.all()[Base_id])
+        except Exception:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        if not arg:
+            print('** class name missing **')
+            return
+        
+        instance = arg.split()
+        if len(instance) == 1:
+            print('** instance id missing **')
+            return
+        try:
+            Base_id = f'{instance[0]}.{instance[1]}'
+            del(storage.all()[Base_id])
+        except Exception:
+            print('** no instance found **')
+
+    def do_all(self, arg):
+        if not arg:
+            for instance in storage.all():
+                print(storage.all()[instance])
+            return
+        try:
+            name_class = eval(arg).__name__
+        except Exception:
+            print('** class doesn\'t exist **')
+        else:
+            instance_list = []
+            instance_str = ""
+            length_storage = len(storage.all())
+            i = 0
+            for instance in storage.all():
+                if instance.startswith(name_class + "."):
+                   instance_str += f'{storage.all()[instance]}'
+            instance_list.append(instance_str)
+            print(instance_list)
+            return
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
